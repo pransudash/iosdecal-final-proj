b4195e78c1374cf1844392251613b62bdefeceed
Pransu Dash <pdash@Pransus-MacBook-Pro.local>
2017-05-02 14:29:15 -0700

    storyboard

---
+++ ClickMore/ImagesCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesCollectionCell.swift
+//  ClickMore
+//
+//  Created by Pransu Dash on 5/2/17.
+//  Copyright © 2017 Pransu Dash. All rights reserved.
+//
+
+import UIKit
+
+class ImagesCollectionCell: UICollectionViewCell {
+    
+}
